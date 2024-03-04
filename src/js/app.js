a5f6ff8f5f352095389ab695023457547a987a0d
dkoncius <as.koncius@gmail.com>
2024-03-04 09:20:46 +0100

    done

--- src/js/app.js
+++ src/js/app.js
@@ -29,7 +29,7 @@ const fetchAndUpdateHomePage = async () => {
         // const metaData = await metaResponse.json();
 
         // Fetch banners data
-        const bannersResponse = await fetch(`${apiUrl}/api/banners?sort=button`);
+        const bannersResponse = await fetch(`${apiUrl}/api/banners?populate=*&sort=button`);
         if (!bannersResponse.ok) throw new Error('Network response was not ok for banners');
         const bannersData = await bannersResponse.json();
 
@@ -40,7 +40,7 @@ const fetchAndUpdateHomePage = async () => {
         // Assuming you have predefined methods to construct HTML for each section
         // const metaHTML = constructMetaHTML(metaData.data)
         const headerHTML = constructHeaderHTML(attributes.cover.data.attributes)
-        // const bannersHTML = constructBannersHTML(bannersData.data);
+        const bannersHTML = constructBannersHTML(bannersData.data);
         const pitchHTML = constructPitchHTML(attributes);
         const iframeHTML = constructIframeHTML(attributes.youtubeLink);
         const galleryHTML = constructGalleryHTML(attributes.galleryImages);
@@ -51,7 +51,7 @@ const fetchAndUpdateHomePage = async () => {
         // Note: This assumes you have placeholders or containers for these sections in your HTML
         // document.head.innerHTML += metaHTML;
         app.innerHTML += headerHTML;
-        // app.innerHTML += bannersHTML;
+        app.innerHTML += bannersHTML;
         app.innerHTML += pitchHTML;
         app.innerHTML += iframeHTML;
         app.innerHTML += galleryHTML;
@@ -117,7 +117,7 @@ function constructBannersHTML(banners) {
      // Dynamically generate banners HTML using fetched data
     const bannersHTML = banners.map((banner) => {
         const { href, button } = banner.attributes
-        let imageUrl = banner.attributes.image.data[0].attributes.url; // Assuming each banner object has attributes with url and alt
+        let imageUrl = banner.attributes.image.data[0].attributes.url;
         imageUrl = `${apiUrl}${imageUrl}`; // Adjust according to your data structure
   
         return `
